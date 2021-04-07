04b41829cb6864aaf2f0d0a0b1aa8912fca0936c
Anton Fadeev <weutnyc@gmail.com>
2021-04-07 21:48:56 +0300

    .

--- PersonalQuiz/ViewControllers/ResultsViewController.swift
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -18,11 +18,6 @@ class ResultsViewController: UIViewController {
     private var values: [Int] = []
     private var maxValue = 0
     
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результат в соответсвии с этим животным
-    // 4. Избавиться от кнопки возврата назад на экране результатов
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.hidesBackButton = true
@@ -71,6 +66,5 @@ class ResultsViewController: UIViewController {
                 descriptionLable.text = String(key.definition)
             }
         }
-        
     }
 }
